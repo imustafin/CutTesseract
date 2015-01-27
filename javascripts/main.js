9fc31459268ae0862981091e062ce8f06179f5d4
gmaksimov <maksimov.grisha@gmail.com>
2015-01-27 18:54:03 +0300

    hide/show
    added script to show and hide menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+function show_hide(id){
+	var item = document.getElementById(id);
+	if (item.style.display == 'none') {item.style.display = 'block';}
+	else item.style.display = 'none';
+}
