d950e2c0aabfe8aa0c194d30bfb752fdf95eaa38
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-01-28 22:53:51 +0300

    Version 0.3

--- src/ru/litsey2/cuttesseract/CubeCut4d.java
+++ src/ru/litsey2/cuttesseract/CubeCut4d.java
@@ -4,6 +4,6 @@ package ru.litsey2.cuttesseract;
 public class CubeCut4d {
 
 	public static void main(String[] args) {
-		MainFrame frame = new MainFrame("CutTesseract WORK");
+		MainFrame frame = new MainFrame("CutTesseract v0.3");
 	}
 }
