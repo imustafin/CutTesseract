2e9eb5fc6c690177e9a3d41d3e3cef408ae3ef26
gmaksimov <maksimov.grisha@gmail.com>
2015-01-28 21:52:55 +0300

    menu top position fixed
    menu top position fixed by js, so now it is header.height + 2 px

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,16 @@
+window.onload = function() {
+	set_menu_distance();
+};
+
 function show_hide(id){
 	var item = document.getElementById(id);
 	if (item.style.display == 'none') {item.style.display = 'block';}
 	else item.style.display = 'none';
+}
+
+function set_menu_distance(){
+	var height = document.getElementById("header").offsetHeight;
+	var div = document.getElementsByClassName("menu");
+	div = div[0];
+	div.style.top = height + 2 + "px";
 }
