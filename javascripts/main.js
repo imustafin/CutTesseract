e28cd77635bb2723272a3f5fc07c4be7d19a3148
gmaksimov <maksimov.grisha@gmail.com>
2015-01-28 23:03:11 +0300

    pre-function added
    pre-js-function to list gallery added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 	set_menu_distance();
+	//leaf_through_gallety("right");
 };
 
 function show_hide(id){
@@ -14,3 +15,31 @@ function set_menu_distance(){
 	div = div[0];
 	div.style.top = height + 2 + "px";
 }
+
+function leaf_through_gallety(where){
+	
+	var images = document.getElementById("gallery").childNodes;
+	var child = [];
+	for(var i = 0; i < images.length; i++){
+		if(images[i].tagName == 'A'){
+			child.push(images[i]);
+		}
+	}
+	if(where == "right"){
+		for(var i = 0; i < child.length - 1; i++){
+			child[i].href = child[i+1].href;
+			var img = child[i].childNodes[0];
+			img.src = child[i+1].href;
+			//console.log(img);
+		}
+	}
+}
+
+/*
+var href = images[i].href;
+var filename = href.substring(href.lastIndexOf('/') + 1, href.lastIndexOf('.'));
+var fileformat = href.substring(href.lastIndexOf('.') + 1);
+var url = href.substring(0, href.lastIndexOf('/'));
+var newname = parseInt(filename.substring(filename.length-1, filename.length))+1;
+console.log(newname);
+*/
