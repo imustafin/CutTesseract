1ff89513eab84ef377beda5b2e3901a97893cb33
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-01-28 22:52:18 +0300

    GUI for point and vector

---
+++ src/ru/litsey2/cuttesseract/PointVectorSelectorPanel.java
@@ -0,0 +1,68 @@
+package ru.litsey2.cuttesseract;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import ru.litsey2.cuttesseract.geometry.Point4d;
+import ru.litsey2.cuttesseract.geometry.Vector4d;
+
+@SuppressWarnings("serial")
+public abstract class PointVectorSelectorPanel extends JPanel {
+
+	Point4d point;
+	Vector4d vector;
+
+	abstract void pointVectorChanged();
+
+	PointVectorSelectorPanel(SegmentDrawer4d segmentDrawer) {
+
+		
+		setPreferredSize(new Dimension(200, 300));
+		
+		setBackground(Color.green);
+
+		SpringLayout layout = new SpringLayout();
+
+		PointPicker pSelector = new PointPicker(Color.LIGHT_GRAY, segmentDrawer) {
+
+			@Override
+			void pointChanged() {
+				point = getPoint4d();
+				pointVectorChanged();
+			}
+		};
+		
+		point = pSelector.getPoint4d();
+		
+		PointPicker vSelector = new PointPicker(Color.CYAN, segmentDrawer) {
+
+			@Override
+			void pointChanged() {
+				vector = new Vector4d(getPoint4d());
+				pointVectorChanged();
+			}
+		};
+
+		vector = new Vector4d(vSelector.getPoint4d());
+		
+		add(pSelector);
+		add(vSelector);
+
+		layout.putConstraint(SpringLayout.SOUTH, pSelector, 0, SpringLayout.NORTH,
+				vSelector);
+		layout.putConstraint(SpringLayout.WEST, pSelector, 0, SpringLayout.WEST,
+				this);
+		layout.putConstraint(SpringLayout.EAST, pSelector, 0, SpringLayout.EAST,
+				this);
+
+		layout.putConstraint(SpringLayout.WEST, vSelector, 0, SpringLayout.WEST,
+				this);
+		layout.putConstraint(SpringLayout.EAST, vSelector, 0, SpringLayout.EAST,
+				this);
+		layout.putConstraint(SpringLayout.SOUTH, vSelector, 0, SpringLayout.SOUTH, this);
+		setLayout(layout);
+	}
+}
